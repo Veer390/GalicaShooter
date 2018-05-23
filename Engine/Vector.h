151815081662d2b440185930c5062d9fd7acab35
sriveer2000 <sriveer2000@gmail.com>
2018-05-23 21:31:27 +0530

    Vector Implemented

---
+++ Engine/Vector.h
@@ -0,0 +1,47 @@
+#pragma once
+
+template<typename T>
+class Vector
+{
+public:
+	//Constructors
+	Vector<T>() = default;
+	//Constructor Which is better to be used...
+	Vector<T>(T x, T y)
+		:
+		x(x),
+		y(y)
+	{}
+	
+	//Main Components X And Y
+public:
+	T x;
+	T y;
+
+	//Operators On the Vector
+public:
+	Vector<T> operator+(Vector<T> rhs)
+	{
+		Vector<T> Temp;
+		Temp.x = x + rhs.x;
+		Temp.y = y + rhs.y;
+		return Temp;
+	}
+
+	Vector<T> operator-(Vector<T> rhs)
+	{
+		Vector<T> Temp;
+		Temp.x = x - rhs.x;
+		Temp.y = y - rhs.y;
+		return Temp;
+	}
+
+	//Scale A Vector
+	Vector<T> operator*(int rhs)
+	{
+		Vector<T> Temp;
+		Temp.x = x*rhs;
+		Temp.y = y * rhs;
+		return Temp;
+	}
+};
